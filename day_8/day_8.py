ab07093c01536a2a881b3a52aa13f788ed2d26cc
lucacorbucci <corbuccilu@gmail.com>
2025-12-08 16:48:45 +0100

    ✨ Day 8 part 2 ✅

--- day_8/day_8.py
+++ day_8/day_8.py
@@ -44,6 +44,9 @@ class DSU:
         # we need to do this to avoid measuring the size of each component
         return sorted([self.size[i] for i in range(len(self.parent)) if self.parent[i] == i], reverse=True)[:k]
 
+    def get_number_of_connected_components(self, n)-> int:
+        return len({self.find(i) for i in range(n)})
+
 class Part1:
     def __init__(self):
         self.distances = []
@@ -66,7 +69,36 @@ class Part1:
 
         return reduce(mul, dsu.top_k_component_sizes(k=3), 1)
 
+class Part2:
+    def __init__(self):
+        self.distances = []
+
+    def compute_distances(self, positions):
+        distances = []
+        for i in range(len(positions)):
+            for j in range(i+1, len(positions)):
+                x_i, y_i, z_i = positions[i]
+                x_j, y_j, z_j = positions[j]
+                distance = math.sqrt((x_i-x_j)**2 + (y_i-y_j)**2 + (z_i-z_j)**2)
+                distances.append((distance, i, j))
+        self.distances = sorted(distances)
+
+    def solution(self, positions) -> int:
+        self.compute_distances(positions)
+        num_positions = len(positions)
+        dsu = DSU(num_positions)
+        for _, i, j in self.distances:
+            dsu.union(i, j)
+            if dsu.get_number_of_connected_components(num_positions) == 1:
+                break
+
+        return positions[i][0]*positions[j][0]
+
+
+
+
 
 positions = parse_input()
 max_junctions = 1000
 print(Part1().solution(positions, max_junctions))
+print(Part2().solution(positions))
